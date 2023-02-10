5a725508c0783bc5be871635c08d0ce45fc1e230
calz18 <40797702+calz18@users.noreply.github.com>
2023-02-10 22:36:11 +0530

    fast api

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from slotmachine import SlotMachine
+from fastapi import FastAPI
 
-# Sample simulations
-slot1 = SlotMachine(3, {"777": 300, "888": 300, "999": 300})
-slot2 = SlotMachine(4, {
-  '7': 1,
-  '77': 10,
-  '777': 150,
-  '7777': 1000
-  })
+app = FastAPI()
 
-print(slot1.simulation(100000, 20000, 2))
-print(slot2.simulation(1000000, 20000, 2))
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+# from slotmachine import SlotMachine
+
+# # Sample simulations
+# slot1 = SlotMachine(3, {"777": 300, "888": 300, "999": 300})
+# slot2 = SlotMachine(4, {
+#   '7': 1,
+#   '77': 10,
+#   '777': 150,
+#   '7777': 1000
+#   })
+
+# print(slot1.simulation(100000, 20000, 2))
+# print(slot2.simulation(1000000, 20000, 2))
