84cc4a0638d7df9c2f6dbae982e2848861891f9e
calz18 <40797702+calz18@users.noreply.github.com>
2023-02-11 11:20:55 +0530

    yo

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 from fastapi import FastAPI
+from slotmachine import SlotMachine
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    slot1 = SlotMachine(3, {"777": 300, "888": 300, "999": 300})
+    return slot1.spin()
 
-# from slotmachine import SlotMachine
 
-# # Sample simulations
+@app.get("/simulation")
+def simulation():
+    slot1 = SlotMachine(3, {"777": 300, "888": 300, "999": 300})
+
+    return slot1.simulation(100000, 20000, 2)
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+# Sample simulations
 # slot1 = SlotMachine(3, {"777": 300, "888": 300, "999": 300})
 # slot2 = SlotMachine(4, {
-#   '7': 1,
-#   '77': 10,
-#   '777': 150,
-#   '7777': 1000
-#   })
+#     '7': 1,
+#     '77': 10,
+#     '777': 150,
+#     '7777': 1000
+# })
 
 # print(slot1.simulation(100000, 20000, 2))
 # print(slot2.simulation(1000000, 20000, 2))
